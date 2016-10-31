74c5eac0202309aa89d6a62d34763a39911305b2
Sigton <sigton.scratch@gmail.com>
2016-10-31 17:12:43 +0000

    Updated things

--- interpreter.py
+++ interpreter.py
@@ -6,4 +6,15 @@ from pgn_parser import parse
 
 code = input("Input your program:\n")
 
-parse(code)
+instructions = parse(code)
+
+'''
+I'm just laying out my thoughts here.
+
+A loop will iterate through the list of parsed instrucions, and executes each one as it goes along.
+Functions will be executed in a function that will run recursively, so that loops within the program
+will be able to run within that single function.
+'''
+
+def execute(instruction):
+    if instruction
