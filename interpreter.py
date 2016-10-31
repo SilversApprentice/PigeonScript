07f88c9d3df071013ef4d5288eb40569ad761529
Sigton <sigton.scratch@gmail.com>
2016-10-31 16:44:24 +0000

    Renamed parser.py to stop errors

--- interpreter.py
+++ interpreter.py
@@ -1,3 +1,9 @@
 # This is the PigeonScript interpreter
 
-from parser import parse
+import pgn-parser
+
+# Take user input and parse it
+
+code = input("Input your program:\n")
+
+parser.parse(code)
