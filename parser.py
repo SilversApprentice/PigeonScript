04245f8c2aeaec08830c26bd17c06feb89721b2b
Sigton <sigton.scratch@gmail.com>
2016-11-02 17:20:41 +0000

    Basiaclly combined joker314 and ScratchMan544's work.

---
+++ parser.py
@@ -0,0 +1,116 @@
+import math
+
+stack = []
+
+digits = list("0123456789")
+set_var = list("ABCDEF")
+get_var = list("abcdef")
+
+# Retrieve data from the stack
+# When the stack is empty, ask for input
+
+def pop():
+    if len(stack):
+        return stack.pop()
+    else:
+        return float(input("Value required: "))
+
+# Define basic maths operations
+
+def add():
+    if len(stack):
+        return pop() + pop()
+    else:
+        return pop() * 2
+
+def sub():
+    if len(stack):
+        return -1 * (pop() - pop())
+    else:
+        return -1 * pop()
+
+def mult():
+    if len(stack):
+        return pop() * pop()
+    else:
+        return math.pow(pop(), 2)
+
+def div():
+    if len(stack):
+        return 1 / (pop() / pop())
+    else:
+        return 1 / pop()
+
+def mod():
+    a,b = pop(),pop()
+    return b % a
+
+# Parse the input into a list of instructions
+
+def parse(code):
+
+    pointer = 0
+
+    c = lambda: code[pointer]
+
+    parsed = []
+
+    while pointer < len(code):
+
+        if c() in digits:
+
+            number = ""
+
+            while pointer < len(code) and (c() in digits or (c() == '.' and '.' not in number)):
+                number += c()
+                pointer += 1
+            pointer -= 1
+            parsed.append(("pushnum", float(number)))
+
+        elif c() in operations:
+            parsed.append(("operation", operations[c()]))
+
+        elif c() in set_var:
+            parsed.append(("setvar", c()))
+
+        elif c() in get_var:
+            parsed.append(("getvar", c()))
+            
+        pointer += 1
+
+    return parsed
+
+operations = {'+':add,
+              '-':sub,
+              '*':mult,
+              '/':div,
+              '%':mod}
+
+scope = {'a':0,
+         'b':0,
+         'c':0,
+         'd':0,
+         'e':0,
+         'f':0}
+
+code = input("Input expression: ")
+
+instructions = parse(code)
+
+print(instructions)
+
+for i in instructions:
+
+    if i[0] == "pushnum":
+        stack.append(i[1])
+
+    elif i[0] == "operation":
+        stack.append(i[1]())
+
+    elif i[0] == "setvar":
+        scope[i[1].lower()] = pop()
+
+    elif i[0] == "getvar":
+        stack.append(scope[i[1]])
+
+print(str(stack))
